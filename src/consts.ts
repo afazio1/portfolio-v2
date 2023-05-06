74613f6a927b5243a18bbaa2c2c5d2f35de1fd33
afazio1 <alexa.fazio04@gmail.com>
2023-05-06 14:40:00 -0400

    add latex support

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Alexa\'s Dev Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome to my dev blog!';
